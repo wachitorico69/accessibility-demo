d4d9d4ccfcad131fa743ebcd7d80d1070b3f64bb
wachitorico69 <morenodario919@gmail.com>
2025-05-20 20:16:41 -0600

    contraste

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -11,9 +11,29 @@ export class HomeComponent {
   barra: boolean = false;
   fontSize: number = 20;
   fontFam: string = "Arial";
+  color: string = "#2ea55a";
+  bcolor: string = "#ff3131";
+  weight: string = "normal";
 
   doSom() {
     this.barra = !this.barra;
+  }
+
+  actContraste() {
+    const colors = ["#2ea55a", "#343232"];
+    const currentIndex = colors.indexOf(this.color);
+    const nextIndex = (currentIndex + 1) % colors.length;
+    this.color = colors[nextIndex];
+
+    const bcolors = ["#ff3131", "#cdc9c5"];
+    const currentIndex2 = bcolors.indexOf(this.bcolor);
+    const nextIndex2 = (currentIndex2 + 1) % bcolors.length;
+    this.bcolor = bcolors[nextIndex2];
+
+    const weights = ["normal", "bolder"];
+    const currentIndex3 = weights.indexOf(this.weight);
+    const nextIndex3 = (currentIndex3 + 1) % weights.length;
+    this.weight = weights[nextIndex3];
   }
 
   changeSize() {
