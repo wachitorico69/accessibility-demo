6c47a61656327445ccabe6d272067a65af4581db
wachitorico69 <morenodario919@gmail.com>
2025-05-20 19:35:13 -0600

    fuente y size

---
+++ src/app/componentes/home/home.component.ts
@@ -0,0 +1,32 @@
+import { NgStyle } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [NgStyle],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  barra: boolean = false;
+  fontSize: number = 20;
+  fontFam: string = "Arial";
+
+  doSom() {
+    this.barra = !this.barra;
+  }
+
+  changeSize() {
+    const sizes = [20, 22, 24, 26];
+    const currentIndex = sizes.indexOf(this.fontSize);
+    const nextIndex = (currentIndex + 1) % sizes.length;
+    this.fontSize = sizes[nextIndex];
+  }
+
+  changeFont() {
+    const fonts = ["Arial", "Verdana", "Roboto"];
+    const currentIndex = fonts.indexOf(this.fontFam);
+    const nextIndex = (currentIndex + 1) % fonts.length;
+    this.fontFam = fonts[nextIndex];
+  }
+}
